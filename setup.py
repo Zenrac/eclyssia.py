806555317fdc32bd0c73897210eb8f5c59b7b08a
Zenrac <zenrac@outlook.fr>
2018-11-03 21:18:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='arcadia',
-    packages=['arcadia'],
+    name='pyarcadia',
+    packages=['pyarcadia'],
     version='1.0.0',
     description='An arcadia-api wrapper built for python3+',
     author='Zenrac',
     author_email='zenrac@outlook.fr',
-    url='https://github.com/Zenrac/Arcadia.py',
-    download_url='https://github.com/Zenrac/Arcadia.py/archive/1.0.0.tar.gz',
-    keywords=['lavalink'],
+    url='https://github.com/Zenrac/arcadia.py',
+    download_url='https://github.com/Zenrac/arcadia.py/archive/1.0.0.tar.gz',
+    keywords=['arcadia'],
     include_package_data=True,
     install_requires=['aiohttp']
 )
