2e8a6a0e1a291eb3092e893fe79fd5a979041b3a
Zenrac <zenrac@outlook.fr>
2019-07-05 01:31:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='pyarcadia',
     packages=['arcadia'],
-    version='1.1.6',
+    version='1.1.7',
     description='An arcadia-api wrapper built for python3+',
     author='Zenrac',
     author_email='zenrac@outlook.fr',
     url='https://github.com/Zenrac/arcadia.py',
-    download_url='https://github.com/Zenrac/arcadia.py/archive/1.1.6.tar.gz',
+    download_url='https://github.com/Zenrac/arcadia.py/archive/1.1.7.tar.gz',
     keywords=['arcadia'],
     include_package_data=True,
     install_requires=['aiohttp']
