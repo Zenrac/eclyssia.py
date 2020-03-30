bf3183223c0b925190d4b14e55e2579533e3e15c
Zenrac <zenrac@outlook.fr>
2020-03-30 18:41:17 +0200

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='eclyssia',
     packages=['eclyssia'],
-    version='1.2.0',
+    version='1.2.1',
     description='An eclyssia-api wrapper built for python3+',
     author='Zenrac',
     author_email='zenrac@outlook.fr',
     url='https://github.com/Zenrac/eclyssia.py',
-    download_url='https://github.com/Zenrac/eclyssia.py/archive/1.2.0.tar.gz',
+    download_url='https://github.com/Zenrac/eclyssia.py/archive/1.2.1.tar.gz',
     keywords=['eclyssia'],
     include_package_data=True,
     install_requires=['aiohttp']
